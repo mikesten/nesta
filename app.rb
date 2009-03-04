b6990d79eeaa6d403f76e2d5c357c0ce962babaa
Mike Stenhouse <mike@trampolinesystems.com>
2009-03-04 01:32:59 +0000

    Moving my custom routes into a separate file. Adding a little content to the static pages. Highlighting the nav.

--- app.rb
+++ app.rb
@@ -2,6 +2,8 @@ require "rubygems"
 require "sinatra"
 require 'sass'
 require 'haml'
+# require 'linguistics'
+# Linguistics::use( :en )
 
 def require_or_load(file)
   if Sinatra::Application.environment == :development
@@ -69,6 +71,8 @@ error do
   haml :error
 end unless Sinatra::Application.environment == :development
 
+load 'dnr.rb'
+
 get "/css/master.css" do
   content_type "text/css", :charset => "utf-8"
   if FileTest.exist?(Nesta::Configuration.stylesheet)
@@ -80,99 +84,6 @@ get "/css/master.css" do
   end
 end
 
-get "/" do
-  set_common_variables
-  @title = "Donotremove aka Mike Stenhouse :: UX, UI and Product Designer"
-  @keywords = "web design, user experience design, user interface design, product design"
-  @description = "This little corner of the interweb is occupied by Mike Stenhouse, a web developer and designer based in London."
-  haml :home
-end
-get "/portfolio" do
-  set_common_variables
-  @title = "Portfolio"
-  @keywords = "web design, user experience design, user interface design, product design, html, xhtml, css, user interface development"
-  @description = ""
-  @portfolio = [
-    {:name => "Persuasive Design, Iceweb 2008", :date => "11/08", :kind => :conference, :role => "Speaker", :skills => "Design and Psychology"},
-    {:name => "Tapping the Mainline, SHiFT 2008", :date => "09/08", :kind => :conference, :role => "Speaker", :skills => "Design and Psychology"},
-    {:name => "From Ego to Ergo, Skillswap", :date => "07/08", :kind => :conference, :role => "Speaker", :skills => "Design and Psychology"},
-    {:name => "But I'm a Bloody Designer, @media 2007", :date => "12/07", :kind => :conference, :role => "Speaker", :skills => "Design, Team Dynamics and Ruby"},
-    {:name => "Collective Intelligence Indeterminacy and the Illusion of Control, ETech 2007", :date => "03/08", :kind => :conference, :role => "Speaker", :skills => "Product Design, Psychology"},
-    {:name => "Building Modern Webapps, The Spring Experience 2007", :date => "12/06", :kind => :conference, :role => "Speaker", :skills => "XHTML, CSS and Javascript"},
-    {:name => "SONAR Knowledge Map, Trampoline Systems", :date => "04/09", :kind => :product, :role => "Head of User Experience", :skills => "Design, User Experience, Product Design, Strategy, Ruby"},
-    {:name => "Out of Five", :date => "01/08", :kind => :product, :role => "Designer and Developer", :skills => "Design, XHTML, CSS, Ruby"},
-    {:name => "Angry Natives", :date => "02/07", :kind => :website, :role => "Designer and Developer", :skills => "Design, XHTML and CSS"},
-    {:name => "Gavin Stenhouse", :date => "01/07", :kind => :website, :role => "Designer and Developer", :skills => "Design, XHTML and CSS"},
-    {:name => "SONAR Dashboard, Trampoline Systems", :date => "09/07", :kind => :product, :role => "Head of User Experience", :skills => "Design, User Experience, Product Design, Strategy, Ruby"},
-    {:name => "SONAR Flightdeck, Trampoline Systems", :date => "08/07", :kind => :product, :role => "Head of User Experience", :skills => "Design, User Experience, Ruby"},
-    {:name => "SONAR Metascope, Trampoline Systems", :date => "06/07", :kind => :product, :role => "Head of User Experience", :skills => "User Experience, Ruby"},
-    {:name => "Enron Explorer, Trampoline Systems", :date => "08/06", :kind => :website, :role => "Head of User Experience", :skills => "XHTML, CSS, DOM Scripting, Design, Usability, Ruby"},
-    {:name => "Bloom Festival 2006", :date => "05/06", :kind => :website, :role => "Designer and Developer", :skills => "Design, CSS, XHTML, PHP"},
-    {:name => "Bottom Drawer", :date => "04/06", :kind => :website, :role => "Developer", :skills => "XHTML, CSS, Accessibility"},
-    {:name => "Keyword Researcher, Wordtracker", :kind => :product, :date => "04/06", :role => "User Experience Consultant", :skills => "XHTML, CSS, Javascript, PHP, User Experience"},
-    {:name => "Will Barras", :date => "05/06", :kind => :website, :role => "Designer", :skills => "Design, XHTML, CSS, PHP"},
-    {:name => "Window on Woking", :date => "02/06", :kind => :website, :role => "Designer", :skills => "Design, CSS"},
-    {:name => "Elizabeth Rodgers Associates", :date => "08/05", :kind => :website, :role => "Developer", :skills => "XHTML, CSS"},
-    {:name => "Guildhall School of Music &amp Drama", :date => "07/05", :kind => :website, :role => "Developer", :skills => "XHTML, CSS, Accessibility, Javascript"},
-    {:name => "Children's Society Audit", :date => "04/05", :kind => :website, :role => "Consultant", :skills => "User Experience, Usability, Strategy"},
-    {:name => "Wordtracker Website, Wordtracker", :date => "04/05 ", :kind => :website, :role => "Developer ", :skills => "XHTML, CSS, Accessibilty, Javascript, PHP"},
-    {:name => "Warwick Arts Center ", :date => "04/05 ", :kind => :website, :role => "Developer ", :skills => "Design, XHTML, CSS, Accessibilty"},
-    {:name => "The Ramblers Association ", :date => "03/05 ", :kind => :website, :role => "Designer and Developer ", :skills => "Design, XHTML, CSS, Accessibilty, Javascript"},
-    {:name => "Lifestuff, Channel 4", :date => "03/05 ", :kind => :website, :role => "Designer and Developer ", :skills => "Design, XHTML, CSS, Accessibilty"},
-    {:name => "Clothing at Tesco ", :date => "03/05 ", :kind => :website, :role => "Consultant ", :skills => "Accessibility"},
-    {:name => "Use Your Paths, The Ramblers Association", :kind => :website, :date => "02/05 ", :role => "Designer and Developer ", :skills => "Design, XHTML, CSS, Accessibilty"},
-    {:name => "International HIV/AIDS Alliance ", :date => "01/05", :kind => :website, :role => "Designer and Developer ", :skills => "Design, XHTML, CSS, ASP, XSLT, Accessibilty, Javascript, User Experience"},
-    {:name => "Content with Style ", :date => "12/04", :kind => :website, :role => "Designer and Developer ", :skills => "Design, XHTML, CSS, Javascript"},
-    {:name => "Webarriba CMS Interface ", :date => "11/04", :kind => :product, :role => "Designer and Developer ", :skills => "Design, XHTML, CSS, Accessibilty, User Experience"},
-    {:name => "SCIE HR ", :date => "11/04 ", :kind => :website, :role => "Designer ", :skills => "CSS, XHTML, Accessibility"},
-    {:name => "PwC Careers ", :date => "11/04 ", :kind => :website, :role => "Developer ", :skills => "HTML, CSS, Javascript"},
-    {:name => "Course Discover ", :date => "10/04", :kind => :website, :role => "Developer ", :skills => "XHTML, CSS, Accessibilty, Javascript"},
-    {:name => "Webarriba ", :date => "10/04 ", :kind => :website, :role => "Designer and Developer ", :skills => "Design, XHTML, CSS, Accessibilty"},
-    {:name => "HAL Knowledge Solutions ", :date => "09/04", :kind => :website, :role => "Developer ", :skills => "XHTML, CSS, Accessibilty"},
-    {:name => "Locus Design ", :date => "08/04 ", :kind => :website, :role => "Developer ", :skills => "XHTML, CSS, Accessibilty"},
-    {:name => "Red Bull Final 5 ", :date => "07/04 ", :kind => :website, :role => "Developer ", :skills => "XHTML, CSS, PHP"},
-    {:name => "Britten-Pears Foundation ", :date => "07/04 ", :kind => :website, :role => "Developer ", :skills => "XHTML, CSS, Accessibilty"},
-    {:name => "English UK ", :date => "07/04 ", :kind => :website, :role => "Developer ", :skills => "XHTML, CSS, Accessibilty"},
-    {:name => "Wafa Trading ", :date => "06/04 ", :kind => :website, :role => "Designer ", :skills => "Design"},
-    {:name => "Spring Barn Vineyard ", :date => "06/04 ", :kind => :website, :role => "Designer and Developer ", :skills => "Design, XHTML, CSS, Accessibilty"},
-    {:name => "Virgin Mobile Louder ", :date => "06/04 ", :kind => :website, :role => "Developer ", :skills => "XHTML, CSS, Accessibilty"},
-    {:name => "Ken4London ", :date => "05/04 ", :kind => :website, :role => "Developer ", :skills => "XHTML, CSS, Accessibilty"},
-    {:name => "Eurovision 2004, BBC", :date => "03/04 ", :kind => :website, :role => "Developer ", :skills => "HTML, CSS"},
-    {:name => "BusinessLink Mk 2 ", :date => "03/04 ", :kind => :website, :role => "Developer ", :skills => "XHTML, CSS, Accessibilty"},
-    {:name => "Postage Paid CSS Zen Garden ", :date => "02/04 ", :kind => :website, :role => "Designer and Developer ", :skills => "Design, CSS"},
-    {:name => "Scooby Doo 2 ", :date => "01/04 ", :kind => :website, :role => "Developer ", :skills => "HTML, CSS"},
-    {:name => "Torque ", :date => "01/04 ", :kind => :website, :role => "Developer ", :skills => "Flash, HTML"},
-    {:name => "Virgin Holidays Email Promotion ", :date => "11/03 ", :kind => :website, :role => "Developer ", :skills => "HTML, CSS"},
-    {:name => "Nykris Training ", :date => "11/03 ", :kind => :training, :role => "Trainer ", :skills => "Training, CSS, XHTML, Accessibility"},
-    {:name => "Interent Car and Van Hire ", :date => "11/03 ", :kind => :website, :role => "Designer and Developer ", :skills => "Design, XHTML, CSS, PHP"},
-    {:name => "BusinessLink Mk 1 ", :date => "09/03 ", :kind => :website, :role => "Developer ", :skills => "XHTML, CSS, Accessibilty"},
-    {:name => "SpiritSoft ", :date => "04/03 ", :kind => :website, :role => "Designer and Developer ", :skills => "CSS, XHTML, ASP, SQL Server, Design"}
-  ]
-  haml :portfolio
-end
-get "/contact" do
-  set_common_variables
-  @title = "Contact"
-  @keywords = ""
-  @description = ""
-  haml :contact
-end
-get "/me" do
-  set_common_variables
-  @title = "Mike Stenhouse"
-  @keywords = ""
-  @description = ""
-  haml :me
-end
-get "/projects" do
-  set_common_variables
-  @title = "Projects"
-  @keywords = ""
-  @description = ""
-  haml :projects
-end
-
-
 get Nesta::Configuration.site_matcher do
   set_common_variables
   @body_class = "home"
@@ -182,6 +93,7 @@ get Nesta::Configuration.site_matcher do
   @keywords = Nesta::Configuration.keywords
   @title = "#{@heading} - #{@subtitle}"
   @articles = Article.find_all[0..7]
+  @nav = :weblog
   haml :index
 end
 
@@ -192,6 +104,7 @@ get Nesta::Configuration.categories_matcher do
   @title = "#{@category.heading} - #{Nesta::Configuration.title}"
   @description = @category.description
   @keywords = @category.keywords
+  @nav = :weblog
   haml :category
 end
 
@@ -207,6 +120,7 @@ get Nesta::Configuration.articles_matcher do
   @description = @article.description
   @keywords = @article.keywords
   @comments = @article.comments
+  @nav = :weblog
   haml :article
 end
 
